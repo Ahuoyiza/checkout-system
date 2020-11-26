09c2663066e9be4b6e352e8e3fc1372d72658810
Ahuoyiza <sophieoyiza@gmail.com>
2020-11-26 23:34:33 +0100

    add action type to cartIcon. BUG: redux connect(store)

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import {connect} from "react-redux";
+
+import {toggleCartHidden } from "../../redux/cart.actions"; 
 
 import {ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.scss';
 
- const CartIcon = () => {
+ const CartIcon = ({toggleCartHidden}) => {
     return (
-        <div className="cart-icon">
+        <div className="cart-icon" onClick={toggleCartHidden}>
             <ShoppingIcon className="shopping-icon"/>
             <span className="item-count"> 0</span>
         </div>
     )
-}
-export default CartIcon
+};
+
+//NEED TO FIX BUG HERE.... ERROR CONECTING TO STORE>> NO PROVIDER  ----TIME TO SUBMIT WILL FIX BUG IF GIVEN AN EXTENSION
+//const mapDispatchToProps = dispatch => ({
+ //   toggleCartHidden: () => dispatch(toggleCartHidden())
+//});
+export default //connect(
+    //null,
+    //mapDispatchToProps
+//) 
+CartIcon;
